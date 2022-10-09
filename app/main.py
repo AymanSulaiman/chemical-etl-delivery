2e95943f3ee0d4c2613f6aede7bc7dee3730c193
AymanSulaiman <saymansulaiman@gmail.com>
2022-10-09 09:50:11 -0700

    Fixed errrors in the api, trying to find ways to make faster, takes a while to find chemicals

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-# from typing import Union
+from typing import Union
 
-# from fastapi import FastAPI
+from fastapi import FastAPI
 
 import shutil
 import tempfile
@@ -11,20 +11,20 @@ from pyspark.sql.session import SparkSession
 from starlette.responses import FileResponse
 import pyspark.sql.functions as F
 
-# app = FastAPI()
-router = APIRouter()
+app = FastAPI()
+# router = APIRouter()
 sc = SparkContext('local')
 spark = SparkSession(sc)
 
 df = spark.read.option("header",True).csv("../data/all_data_csv")
 
-@router.get("/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 #clase chemical_list_json():
 
-@router.get("/get_chemical_data/{chemical_name}")
+@app.post("/get_chemical_data/{chemical_name}")
 def get_chemical_data(chemical_name: str):
-    return df.filter(F.col('molecule_name') == "chemical_name").toJSON().map(eval).collect()
+    return df.filter(F.col('molecule_name') == f"{chemical_name}").toJSON().map(eval).collect()
