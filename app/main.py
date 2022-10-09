2a00d8d45f1752fd30132d439fb456da79edcc58
AymanSulaiman <saymansulaiman@gmail.com>
2022-10-09 09:36:15 -0700

    Started building API backbone, testing now

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-from typing import Union
+# from typing import Union
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+import shutil
+import tempfile
 
+from fastapi import APIRouter
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from starlette.responses import FileResponse
+import pyspark.sql.functions as F
 
-@app.get("/")
+# app = FastAPI()
+router = APIRouter()
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
+df = spark.read.option("header",True).csv("../data/all_data_csv")
+
+@router.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+#clase chemical_list_json():
+
+@router.get("/get_chemical_data/{chemical_name}")
+def get_chemical_data(chemical_name: str):
+    return df.filter(F.col('molecule_name') == "chemical_name").toJSON().map(eval).collect()
